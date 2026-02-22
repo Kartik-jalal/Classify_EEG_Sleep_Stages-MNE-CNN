0656fc7e2dabd11ca4832928691943fe31f41c7a
kartik-jalal <kartikjalal@gmail.com>
2026-02-22 16:25:51 +0000

    updated the docstring

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -5,7 +5,7 @@ This module serves as a package initializer for the `src.datasets` package.
 It imports and exposes dataset class, split function that are used across the project.
 
 ##### Author: Kartik M. Jalal
-##### Last Updated: 02-20-2026
+##### Last Updated: 02-22-2026
 """
 
 from .epochs import (
@@ -18,7 +18,7 @@ from .split import (
 
 __all__ = [
     # from epochs.py
-    "EpochsDataset"
+    "EpochsDataset",
     # from split.py
     "split_by_subject"
 ]
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,7 +6,7 @@ It imports and exposes utility functions that are used across the project,
 particularly those related to data loading and preprocessing for sleep staging.
 
 ##### Author: Kartik M. Jalal
-##### Last Updated: 02-21-2026
+##### Last Updated: 02-22-2026
 """
 
 from .data_loader import (
@@ -20,7 +20,7 @@ from .preprocessing import (
 
 __all__ = [
     # from data_loader.py
-    "load_sleep_physionet_raw_data"
+    "load_sleep_physionet_raw_data",
     # from preprocessing.py
     "extract_epochs",
     "scale_epoch"
