d01af5b1acb66dfc3c65a77119f86751997a2b69
kartik-jalal <kartikjalal@gmail.com>
2026-03-04 10:01:54 +0000

    updated the naming scheme of few variables

--- src/datasets/split.py
+++ src/datasets/split.py
@@ -5,7 +5,7 @@ This module contains functions for randomly splits a dataset into Train, Validat
 Test sets based on SUBJECT ID.
 
 ##### Author: Kartik M. Jalal
-##### Last Updated: 02-20-2026
+##### Last Updated: 03-04-2026
 """
 
 
@@ -16,8 +16,8 @@ from typing import Tuple
 
 def split_by_subject(
     dataset : ConcatDataset,
-    n_subjects_test : int,
-    n_subjects_val : int,
+    n_test_subjects : int,
+    n_val_subjects : int,
     seed : int = 42
 ) -> Tuple[ConcatDataset, ConcatDataset, ConcatDataset] :
     """
@@ -28,9 +28,9 @@ def split_by_subject(
     ----------
     - dataset : ConcatDataset
         The dataset to split.
-    - n_subject_test : int
+    - n_test_subjects : int
         Number of unique subjects to set aside for test.
-    - n_subject_val : int
+    - n_val_subjects : int
         Number of unique subjects to set aside for validation.
     - seed : int
         Random seed for reproducibility.
@@ -51,11 +51,11 @@ def split_by_subject(
     shuffled_subjects = rng.permutation(unique_subjects) # random shuffle
 
     # select subjects for test, validation and train
-    test_subjects = shuffled_subjects[:n_subjects_test]
+    test_subjects = shuffled_subjects[:n_test_subjects]
     val_subjects = shuffled_subjects[
-        n_subjects_test : n_subjects_test + n_subjects_val
+        n_test_subjects : n_test_subjects + n_val_subjects
     ]
-    train_subjects = shuffled_subjects[n_subjects_test + n_subjects_val :]
+    train_subjects = shuffled_subjects[n_test_subjects + n_val_subjects :]
 
     # get the orig indices of the subjects for each split
     test_subjects_idx = [
